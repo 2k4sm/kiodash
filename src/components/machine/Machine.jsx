6890d0cfc681a2911b6f86b83f2b21e563891a62
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-10-05 12:51:52 +0530

    impr: adds a dummy websocket connection for random data sending-recieving and updating the machine state according to that.

--- src/components/machine/Machine.jsx
+++ src/components/machine/Machine.jsx
@@ -1,5 +1,5 @@
 import { useDispenserStore, useRecipeStore } from "../../store/store";
-import { useMemo, useState } from "react";
+import { useMemo, useState, useEffect } from "react";
 import ViewMachine from "./ViewMachine";
 
 function Machine({ machine, machineId }) {
@@ -13,10 +13,51 @@ function Machine({ machine, machineId }) {
 
     const [isMachineOpen, setMachine] = useState(false)
 
+    const [strokeColor, setStrokeColor] = useState("green");
+
+    useEffect(() => {
+        const getRandomColor = (data) => {
+            const colors = ["green", "red", "yellow"];
+            return colors[Math.floor(Math.random() * data % colors.length)];
+        };
+
+        const ws = new WebSocket("wss://ws.postman-echo.com/raw");
+
+        ws.onopen = () => {
+            console.log("Connected to Postman Echo WebSocket server");
+
+            const intervalId = setInterval(() => {
+                const randomNumber = Math.floor(Math.random() * 100);
+                ws.send(randomNumber.toString());
+            }, 1000);
+
+            return () => clearInterval(intervalId);
+        };
+
+        ws.onmessage = (event) => {
+            console.log("Received from server:", event.data);
+            setStrokeColor(getRandomColor(event.data));
+        };
+
+        ws.onerror = (error) => {
+            console.error("WebSocket error:", error);
+        };
+
+        ws.onclose = (event) => {
+            console.log("WebSocket connection closed:", event);
+        };
+
+        return () => {
+            ws.close();
+        };
+    }, []);
+
+
+
     return (
         <div className="card card-compact bg-base-100 w-full md:w-[30vw] lg:w-[24vw] shadow-xl min-w-[5vw]">
             <div className="flex flex-col p-2 justify-center sm:p-4 md:p-6 lg:p-8 gap-2">
-                <svg className="w-8 h-8 sm:w-12 sm:h-12 md:w-14 md:h-14 lg:w-16 lg:h-16" stroke="green" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+                <svg className="w-8 h-8 sm:w-12 sm:h-12 md:w-14 md:h-14 lg:w-16 lg:h-16" stroke={strokeColor} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
                     <line x1="22" y1="12" x2="2" y2="12"></line>
                     <path d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"></path>
                     <line x1="6" y1="16" x2="6.01" y2="16"></line>
